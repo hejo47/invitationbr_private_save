b2d1ba2602218fb2717db32583c4f3b305e69dc3
hejo47 <hejo_47@naver.com>
2024-12-30 11:38:55 +0900

    feat: 모바일 헤더 인디케이터 처리_241230

--- src/components/sub/SubHeader.js
+++ src/components/sub/SubHeader.js
@@ -1,5 +1,5 @@
 /* import */
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 /* CSS Module */
 import styles from "../../css/module/sub/SubHeader.module.scss";
@@ -8,6 +8,7 @@ import { SetContext } from "../../store/option-set-context.js";
 /* Image */
 import logoImg from "../../img/main/logo.svg";
 import menuImg from "../../img/icon/icon_header_menu.png";
+import rightImg from "../../img/icon/icon_arrow_right.png";
 import closeImg from "../../img/icon/icon_header_menu_close.png";
 import myInfo from "../../img/icon/icon_info.png";
 
@@ -15,9 +16,38 @@ const SubHeader = () => {
   const { isMobile } = useContext(SetContext);
   const [ activeMenu, setActiveMenu ] = useState(false);
   const location = useLocation();
+  const moHeaderRef = useRef(null);
+  const headerRef = useRef(null);
+  const [lastScroll, setLastScroll] = useState(0);
+  const handleScroll = () => {
+    const currentScroll = window.scrollY;
+    if (currentScroll > 0) {
+      headerRef.current.classList.add(styles.active);
+      moHeaderRef.current.style.display = "flex";
+    } else if (currentScroll <= 0) {
+      moHeaderRef.current.style.display = "none";
+    } else {
+      headerRef.current.classList.remove(styles.active);
+    }
+    if (lastScroll > currentScroll) {
+      headerRef.current.style.display = "flex"; 
+    } else if (lastScroll < currentScroll) {
+      headerRef.current.style.display = "none"; 
+      headerRef.current.classList.remove(styles.active); 
+    }
+    setLastScroll(currentScroll);
+  };
+  useEffect(() => {
+    if (isMobile) {
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll); 
+      };
+    }
+  }, [isMobile, lastScroll]); // 의존성 추가
   return (
     <div className={`${styles.sub__header} ${activeMenu && isMobile ? styles["active"] : ""}`}>
-      <div className={`${styles.sub__header_wrapper}`}>
+      <div className={`${styles.sub__header_wrapper} ${styles.active}`} ref={headerRef}>
         <Link to="/Manager" className={styles.logo}>
           <img src={logoImg} alt="시집가는 날" />
         </Link>
@@ -45,6 +75,19 @@ const SubHeader = () => {
           </button>
         )}
       </div>
+      {
+        isMobile ? 
+        <div 
+          className={`${styles.sub__header_wrapper} ${styles.mo} ${styles.active}`} 
+          ref={moHeaderRef}
+          style={{display: "none"}}
+        >
+          <Link to="/Manager" className={styles.logo}>
+            <img src={rightImg} alt="시집가는 날" />
+          </Link>
+          <h2>공지사항</h2>
+        </div> : null
+      }
     </div>
   )
 }
