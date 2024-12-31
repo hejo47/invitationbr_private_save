d823950b5c7378962d909210f7511ca2c5b1d8b5
hejo47 <hejo_47@naver.com>
2024-12-31 14:55:30 +0900

    feat: 로고 변경_241231

--- src/components/page/Join.js
+++ src/components/page/Join.js
@@ -1,6 +1,6 @@
 /* import */
 import { useContext } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import ReactDOM from "react-dom";
 /* CSS */
 import styles from "../../css/module/page/Join.module.scss";
@@ -22,41 +22,80 @@ const Join = () => {
         subContent="간편하게 가입하고 무료 모바일청첩장 서비스를 이용하세요."
       />
       {/* <ManageList></ManageList> */}
-      <SubWrapper>
-        <div className="card">
-          <div className="card__wrap">
-            <div className="card__title">
+      <SubWrapper column>
+        <div className={styles.card}>
+          <div className={styles.card__wrap}>
+            <div className={styles.card__title}>
               <h4>약관동의<span>*</span></h4>
             </div>
-            <div className="card__content">
+            <div className={styles.card__content}>
 
-              <div>
-                <div>
-                  <input type="text" />
-                  <label htmlFor=""></label>
+              <div className={styles.card__item}>
+                <div className={styles.card__chkbox}>
+                  <input type="checkbox" />
+                  <label htmlFor="">모두 동의</label>
                 </div>
               </div>
 
-              <div>
-                <div>
-                  <input type="text" />
-                  <label htmlFor=""></label>
+              <div className={styles.card__item}>
+                <div className={styles.card__chkbox}>
+                  <input type="checkbox" />
+                  <label htmlFor="">회원가입약관 동의(필수)</label>
                 </div>
-                <span>약관보기</span>
+                <Link to="/">약관보기</Link>
               </div>
-              <div>
-                <div>
-                  <input type="text" />
-                  <label htmlFor=""></label>
+              <div className={styles.card__item}>
+                <div className={styles.card__chkbox}>
+                  <input type="checkbox" />
+                  <label htmlFor="">개인정보처리방침안내의 내용(필수)</label>
                 </div>
-                <span>약관보기</span>
+                <Link to="/">약관보기</Link>
               </div>
-
-              <ButtonWrapper>
-                <Button content="휴대폰 본인 인증"></Button>
-                <span>휴대폰을 이용하여 본인인증을 진행합니다.</span>
-              </ButtonWrapper>
+              
             </div>
+            <ButtonWrapper styleType="center">
+              <Button content="휴대폰 본인 인증" styleType="point"></Button>
+            </ButtonWrapper>
+
+            <small>휴대폰을 이용하여 본인인증을 진행합니다.</small>
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <div className={styles.card__wrap}>
+            <div className={styles.card__title}>
+              <h4>회원정보<span>*</span></h4>
+            </div>
+            <div className={styles.card__content}>
+
+              <div className={styles.card__item}>
+                <div className={styles.card__text}>
+                  <input type="text" />
+                  <label htmlFor="">이름</label>
+                </div>
+              </div>
+
+              <div className={styles.card__item}>
+                <div className={styles.card__text}>
+                  <input type="checkbox" />
+                  <label htmlFor="">회원가입약관 동의(필수)</label>
+                </div>
+                <Link to="/">약관보기</Link>
+              </div>
+              <div className={styles.card__item}>
+                <div className={styles.card__text}>
+                  <input type="checkbox" />
+                  <label htmlFor="">개인정보처리방침안내의 내용(필수)</label>
+                </div>
+                <Link to="/">약관보기</Link>
+              </div>
+              
+            </div>
+            <ButtonWrapper styleType="center">
+              <Button content="휴대폰 본인 인증" styleType="point"></Button>
+            </ButtonWrapper>
+
+            <small>휴대폰을 이용하여 본인인증을 진행합니다.</small>
           </div>
         </div>
       </SubWrapper>
